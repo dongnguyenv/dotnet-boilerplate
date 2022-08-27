9f2b594cee761c56fdfd80838223429091791632
levi-nguyen <nvdong1989@gmail.com>
2022-08-27 13:53:42 +0700

    update connection string

--- src/LN.Infrastructure/Persistence/TenantAppDbContext.cs
+++ src/LN.Infrastructure/Persistence/TenantAppDbContext.cs
@@ -17,7 +17,6 @@ namespace LN.Infrastructure.Persistence
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // optionsBuilder.UseSqlServer(_connectionString);
             base.OnConfiguring(optionsBuilder);
         }
     }
